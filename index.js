634a04f4fc5d25c3dc50b27bf7409b08cb9e45e8
mauricioNascimentoCode <152783201+mauricioNascimentoCode@users.noreply.github.com>
2023-12-09 23:14:47 -0300

    Meu Primeiro Commit

--- index.js
+++ index.js
@@ -4,26 +4,26 @@ let pontosXp =   prompt("Digite os pontos EXP")
 let nivelHeroi = ["Ferro" , "Bronze" , "Prata" , "Ouro" , "Platina" , "Ascendente" , "Imortal" , "Radiante"]
 
             if (pontosXp < 1000 ) {
-            console.log ( "O Herói de nome " +  nomeHeroi + " está no nível de " + nivelHeroi[0])
+            console.log ( "O Herói de nome " +  nomeHeroi + " está no nivel de " + nivelHeroi[0])
             }
              else if (pontosXp >= 1001 && pontosXp <= 2000) {
-            console.log ( "O Herói de nome " +  nomeHeroi + " está no nível de " + nivelHeroi[1])
+            console.log ( "O Herói de nome " +  nomeHeroi + " está no nivel de " + nivelHeroi[1])
             }
              else if (pontosXp >= 2001 && pontosXp <= 5000) {
-            console.log ( "O Herói de nome " +  nomeHeroi + " está no nível de " + nivelHeroi[2])
+            console.log ( "O Herói de nome " +  nomeHeroi + " está no nivel de " + nivelHeroi[2])
              }
              else if (pontosXp >= 6001 && pontosXp <= 7000) {
-            console.log ( "O Herói de nome " +  nomeHeroi + " está no nível de " + nivelHeroi[3])
+            console.log ( "O Herói de nome " +  nomeHeroi + " está no nivel de " + nivelHeroi[3])
              }
              else if (pontosXp >= 7001 && pontosXp <= 8000) {
-            console.log ( "O Herói de nome " +  nomeHeroi + " está no nível de " + nivelHeroi[4])
+            console.log ( "O Herói de nome " +  nomeHeroi + " está no nivel de " + nivelHeroi[4])
              }
              else if (pontosXp >= 8001 && pontosXp <= 9000) {
-            console.log ( "O Herói de nome " +  nomeHeroi + " está no nível de " + nivelHeroi[5])
+            console.log ( "O Herói de nome " +  nomeHeroi + " está no nivel de " + nivelHeroi[5])
              }
              else if (pontosXp >= 9001 && pontosXp <= 10000) {
-            console.log ( "O Herói de nome " +  nomeHeroi + " está no nível de " + nivelHeroi[6])
+            console.log ( "O Herói de nome " +  nomeHeroi + " está no nivel de " + nivelHeroi[6])
              }
              else if (pontosXp >= 10000) {
-            console.log ( "O Herói de nome " +  nomeHeroi + " está no nível de " + nivelHeroi[7])
+            console.log ( "O Herói de nome " +  nomeHeroi + " está no nivel de " + nivelHeroi[7])
             }
